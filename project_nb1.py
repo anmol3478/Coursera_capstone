0023f9bf2ad8fbb732e44480efe644603b0c6bd6
anmol3478 <56272685+anmol3478@users.noreply.github.com>
2019-10-08 00:23:28 +0530

    Add files via upload

---
+++ project_nb1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Hello, this notebook will primarily be used for the IBM Applied data science capstone project
+# 
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
